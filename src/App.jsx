9d2f4753127371da887dc01006951a719e74a4c0
vidyajaggi <vidyajaggi05@gmail.com>
2025-12-03 09:52:44 +0530

    inc&dec

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
+import { useState } from "react";
+
 function App() {
-  const user = "Vidya"
-  const abc = () =>{
-    console.log("hello",user)
-  }
+  const [a,setA] = useState(10)
 
   return (
   <div>
-    <h1>Hello1</h1>
-    <h2>Hello {user}</h2>
-    <button onClick={abc}>Change User</button>
+    <h1>Value of a is:{a}</h1>
+    <button onClick={()=>setA(a+10)}>Increment</button>
+    <button onClick={()=>setA(a-10)}>Decrement</button>
   </div> 
   );
 }
