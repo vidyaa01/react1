2037248288012cbefc93b90a5e821a17eed3abc8
vidyajaggi <vidyajaggi05@gmail.com>
2025-12-04 10:18:41 +0530

    tailwind-error

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
+import './App.css'
 
 function App() {
   const [a,setA] = useState(10)
 
   return (
   <div>
-    <h1>Value of a is:{a}</h1>
-    <button onClick={()=>setA(a+10)}>Increment</button>
-    <button onClick={()=>setA(a-10)}>Decrement</button>
+    <h1 className="text-5xl bg-green-500">Hello</h1>
   </div> 
   );
 }
